a152cf282a3de4e35ebffb899ca8f7142e596483
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-29 21:25:38 +0100

    Sync local changes

--- Sources/Binary Primitives/Binary.Parse.Error.swift
+++ Sources/Binary Primitives/Binary.Parse.Error.swift
@@ -3,12 +3,14 @@
 //
 // Error type for whole-buffer parsing.
 
+public import Index_Primitives
+
 extension Binary.Parse {
     /// Error indicating parsing did not consume entire input.
     public enum Error: Swift.Error, Sendable, Equatable {
         /// Parsing succeeded but bytes remain.
         ///
         /// - Parameter remaining: The number of unconsumed bytes.
-        case end(remaining: Int)
+        case end(remaining: Index<UInt8>.Count)
     }
 }
